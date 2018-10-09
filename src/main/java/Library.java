039a169f47edde98126a08a00095910dbe9d2030
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-09 17:49:22 +0100

    Add library and count books method

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Library {
+    ArrayList<Book> books;
+    int capacity;
+
+    Library(ArrayList<Book> books, int capacity) {
+        this.books = books;
+        this.capacity = capacity;
+    }
+
+    public int countBooks() {
+        return this.books.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class LibraryTest {
+
+    Book book1;
+    Book book2;
+    ArrayList<Book> books;
+    Library library;
+
+    @Before
+    public void before() {
+        book1 = new Book("Foundation", "Isaac Asimov", "sci-fi");
+        book2 = new Book("Hamlet", "William Shakespeare", "tragedy");
+        books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+        library = new Library(books, 3);
+    }
+
+    @Test
+    public void countBooks() {
+        assertEquals(2, library.countBooks());
+    }
 }
